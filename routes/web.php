7a4dcd175f79eee559076bd9dc5e0843f24a8ae2
Noah Falk <hi@noahfalk.com>
2021-07-25 19:06:37 +0200

    Setup must be complete and feed authentication checked for displaying feed and media files

--- routes/web.php
+++ routes/web.php
@@ -9,28 +9,19 @@ use App\Http\Controllers\FeedController;
 use App\Http\Controllers\AudiobookController;
 use App\Http\Controllers\FileDownloadController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::middleware([EnsureSetupIsComplete::class, 'auth.web'])->group(function () {
     Route::get('/', [DashboardController::class, 'index']);
     Route::get('/dashboard', [DashboardController::class, 'index']);
     Route::get('/audiobooks', [DashboardController::class, 'index']);
     Route::get('/audiobooks/{audiobook:slug}', [AudiobookController::class, 'show']);
+});
+
+Route::middleware([EnsureSetupIsComplete::class, 'auth.feed'])->group(function () {
+    Route::get('/feed', FeedController::class);
     Route::get('/audiobooks/{audiobook:slug}/media/{file:id}', FileDownloadController::class);
 });
 
-Route::get('/feed', FeedController::class)->middleware('auth.feed');
 
-// TODO: Setup must be incomplete
 Route::get('/setup', function () {
     return view('setup');
 })->middleware(EnsureSetupIsIncomplete::class);
